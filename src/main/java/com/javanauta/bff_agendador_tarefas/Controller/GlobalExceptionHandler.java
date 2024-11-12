0665e65ae90ae11c7ada34e91869905446ab2472
João <joaolucassantos.dev@gmail.com>
2024-11-12 19:51:28 -0300

    projeto 100% atualizado

---
+++ src/main/java/com/javanauta/bff_agendador_tarefas/Controller/GlobalExceptionHandler.java
@@ -0,0 +1,30 @@
+package com.javanauta.bff_agendador_tarefas.Controller;
+
+
+
+import com.javanauta.bff_agendador_tarefas.Infrastructure.Exceptions.ConflictException;
+import com.javanauta.bff_agendador_tarefas.Infrastructure.Exceptions.ResourceNotFoundException;
+import com.javanauta.bff_agendador_tarefas.Infrastructure.Exceptions.UnauthorizedException;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+@ControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(ResourceNotFoundException.class)
+    public ResponseEntity<String> handleResourceNotFoundException(ResourceNotFoundException ex){
+        return new ResponseEntity<>(ex.getMessage(), HttpStatus.NOT_FOUND);
+    }
+
+    @ExceptionHandler(ConflictException.class)
+    public ResponseEntity<String> handleConflictException(ConflictException ex){
+        return new ResponseEntity<>(ex.getMessage(), HttpStatus.CONFLICT);
+    }
+
+    @ExceptionHandler(UnauthorizedException.class)
+    public ResponseEntity<String> handleUnauthorizedException(UnauthorizedException ex){
+        return new ResponseEntity<>(ex.getMessage(), HttpStatus.UNAUTHORIZED);
+    }
+}
